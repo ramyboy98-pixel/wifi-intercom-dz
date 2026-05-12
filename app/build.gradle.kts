7b350687c06be9ca07c6793f7340d965ba5d24d1
ramyboy98-pixel <ramyboy98@gmail.com>
2026-05-12 10:55:21 +0100

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,12 +16,17 @@ android {
 
         targetSdk = 36
 
-        versionCode = 2
+        versionCode = 3
 
-        versionName = "2.0"
+        versionName = "3.0"
     }
 
     buildTypes {
+
+        debug {
+
+            isDebuggable = true
+        }
 
         release {
 
@@ -31,8 +36,10 @@ android {
 
     compileOptions {
 
-        sourceCompatibility = JavaVersion.VERSION_17
+        sourceCompatibility =
+            JavaVersion.VERSION_17
 
-        targetCompatibility = JavaVersion.VERSION_17
+        targetCompatibility =
+            JavaVersion.VERSION_17
     }
 }
