069ba120a30575ed5d13901812ab670271dc34df
ramyboy98-pixel <ramyboy98@gmail.com>
2026-05-12 11:59:57 +0100

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -3,68 +3,30 @@ plugins {
 }
 
 android {
-
     namespace = "com.idaradz.wifiintercom"
-
     compileSdk = 36
 
     defaultConfig {
-
         applicationId = "com.idaradz.wifiintercom"
-
         minSdk = 24
-
         targetSdk = 36
-
-        versionCode = 6
-
-        versionName = "6.0"
+        versionCode = 7
+        versionName = "7.0"
     }
 
     buildTypes {
-
         debug {
-
             isDebuggable = true
         }
 
         release {
-
             isMinifyEnabled = false
-
             isShrinkResources = false
         }
     }
 
     compileOptions {
-
-        sourceCompatibility =
-            JavaVersion.VERSION_17
-
-        targetCompatibility =
-            JavaVersion.VERSION_17
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
-
-    packaging {
-
-        resources {
-
-            excludes += "/META-INF/{AL2.0,LGPL2.1}"
-        }
-    }
-}
-
-dependencies {
-
-    implementation(
-        "androidx.core:core:1.13.1"
-    )
-
-    implementation(
-        "androidx.appcompat:appcompat:1.7.0"
-    )
-
-    implementation(
-        "com.google.android.material:material:1.12.0"
-    )
 }
